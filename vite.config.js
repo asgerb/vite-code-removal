b459a210bd44ed1beca4905e69f4d342989f9b57
asgerb <a@asgerbehnckejacobsen.dk>
2023-09-05 13:17:06 +0200

    Use vite starter template

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,12 @@
-import { resolve } from "path"
 import { defineConfig } from "vite"
 
 export default defineConfig({
   build: {
-    outDir: "dist",
-    cssCodeSplit: true,
-    clearOutputPath: true,
     minify: false,
     lib: {
-      entry: {
-        admin: resolve(__dirname, "admin.js"),
-      },
-      name: "@asgerb/vite-code-removal",
+      entry: "./lib/main.js",
+      name: "ViteCodeRemoval",
+      fileName: "main",
     },
   },
 })
